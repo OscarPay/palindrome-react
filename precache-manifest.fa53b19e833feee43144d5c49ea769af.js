0b0ccc5f625454e72e1bfc00e02f3b36fb0452af
oscar <oscar.pay4@gmail.com>
2020-07-25 10:17:30 -0500

    Updates

---
+++ precache-manifest.fa53b19e833feee43144d5c49ea769af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68cc82861d5b0932a6b9dc9f91443714",
+    "url": "/palindrome-react/index.html"
+  },
+  {
+    "revision": "48f51780de2f17c66782",
+    "url": "/palindrome-react/static/css/main.b5372168.chunk.css"
+  },
+  {
+    "revision": "017000dad2362e1844b5",
+    "url": "/palindrome-react/static/js/2.c97f3f64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/palindrome-react/static/js/2.c97f3f64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48f51780de2f17c66782",
+    "url": "/palindrome-react/static/js/main.8e7e26f3.chunk.js"
+  },
+  {
+    "revision": "1b1b1de52f4beb32d5e0",
+    "url": "/palindrome-react/static/js/runtime-main.ab2eeb83.js"
+  }
+]);
